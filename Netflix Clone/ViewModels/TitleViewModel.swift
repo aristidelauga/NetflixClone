a9d4a39ba230664ca35b5110cc686675515b8bff
aristidelauga <aristide.lauga@gmail.com>
2022-08-21 12:45:08 +0200

    API Calls for Images set in Upcoming views

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Aristide LAUGA on 21/08/2022.
+//
+
+import Foundation
+
+struct TitleViewModel {
+  let titleName: String
+  let posterURL: String
+}
